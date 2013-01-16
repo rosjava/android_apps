113e9fc8d9e61c1c377240f62870275826439878
Brian Gerkey <gerkey@osrfoundation.org>
2013-01-16 15:35:31 -0800

    modify configure.py to process all project directories when given no args

--- tools/configure.py
+++ tools/configure.py
@@ -14,28 +14,31 @@ android_core_path = subprocess.check_output(['rosstack', 'find', 'android_core']
 # ant/Android requires the path to be relative for some reason
 android_core_relpath = os.path.relpath(android_core_path, cwd)
 
-USAGE = 'configure.py <proj_dir>'
+USAGE = 'configure.py [<proj_dir>]'
 
 def parse_args(argv):
-    if len(argv) != 2:
-        print(USAGE)
-        sys.exit(1)
-    args = {}    
-    args['proj'] = argv[1]
-    return args
+    dirs = []
+    if len(argv) > 1:
+        dirs = argv[1:]
+    else:
+        # Find directories that appear to be Android projects
+        for f in os.listdir('.'):
+            if os.path.isdir(f) and os.path.exists(os.path.join(f, 'AndroidManifest.xml')):
+                dirs.append(f)
+    return dirs
 
 def go(argv):
-    args = parse_args(argv)
-    p = args['proj']
-
-    p_path = os.path.join(cwd, p)
-    prop_path = os.path.join(p_path, properties_fname)
-    if os.path.exists(prop_path):
-        os.unlink(prop_path)
-    for l in other_libs:
-        l_path = os.path.join('..', android_core_relpath, l)
-        cmd = ['android', 'update', 'project', '--path', p_path, '--target', android_version, '--library', l_path]
-        subprocess.check_call(cmd)
+    dirs = parse_args(argv)
+    for p in dirs:
+        print('Operating on: %s'%(p))
+        p_path = os.path.join(cwd, p)
+        prop_path = os.path.join(p_path, properties_fname)
+        if os.path.exists(prop_path):
+            os.unlink(prop_path)
+        for l in other_libs:
+            l_path = os.path.join('..', android_core_relpath, l)
+            cmd = ['android', 'update', 'project', '--path', p_path, '--target', android_version, '--library', l_path]
+            subprocess.check_call(cmd)
 
 if __name__ == '__main__':
     go(sys.argv)
