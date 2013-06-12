d399a91c48571a701ec49745f067123914b11eb7
Daniel Stonier <d.stonier@gmail.com>
2013-06-12 10:46:30 +0900

    get direct application connections to simply pick up robot name and type information.

--- application_management/src/main/java/com/github/ros_java/android_apps/application_management/RobotNameResolver.java
+++ application_management/src/main/java/com/github/ros_java/android_apps/application_management/RobotNameResolver.java
@@ -1,9 +1,13 @@
 package com.github.ros_java.android_apps.application_management;
 
+import android.util.Log;
+
 import org.ros.namespace.GraphName;
 import org.ros.namespace.NameResolver;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
+import org.ros.node.parameter.ParameterTree;
+import org.ros.exception.ParameterClassCastException;
 
 public class RobotNameResolver extends AbstractNodeMain {
 
@@ -44,12 +48,30 @@ public class RobotNameResolver extends AbstractNodeMain {
 	}
 
 	@Override
+    /**
+     * Resolves the namespace under which robot apps can be started
+     * and stopped. Sometimes this will already have been provided
+     * via setRobot(), for example by appchooser or remocons which
+     * use the MasterChecker.
+     *
+     * In other cases, such as when the
+     * application directly connects, we do a simple parameter
+     * lookup, falling back to a default if provided.
+     */
 	public void onStart(final ConnectedNode connectedNode) {
 		this.connectedNode = connectedNode;
 		if (currentRobot != null) {
 			name = GraphName.of(currentRobot.getRobotName());
-		}
-			app = name.join(GraphName.of("application"));
+		} else {
+            // Could actually construct a RobotDescription here if we wished.
+            ParameterTree parameterTree = this.connectedNode.getParameterTree();
+            try {
+                name = GraphName.of(parameterTree.getString("/robot/name"));
+            } catch (ParameterClassCastException e) {
+                Log.i("RosApplicationManagement", "Couldn't find the robot name on the parameter server, falling back to defaults.");
+            }
+        }
+		app = name.join(GraphName.of("application"));
 		appNameResolver = connectedNode.getResolver().newChild(app);
 		robotNameResolver = connectedNode.getResolver().newChild(name);
 	}
