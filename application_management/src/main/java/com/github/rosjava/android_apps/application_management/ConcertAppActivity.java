daf2239d512816d6d3ae444bc8593219a39e0e4e
Jorge Santos <jsantossimon@gmail.com>
2013-11-07 17:01:32 +0900

    Three classes added to allow apps to work as concert apps. They must be refactored together with robot equivalents
    Three classes added to allow apps to work as concert apps. They must be refactored together with robot equivalents.
    
    Three classes added to allow apps to work as concert apps. They must be refactored together with robot equivalents

---
+++ application_management/src/main/java/com/github/rosjava/android_apps/application_management/ConcertAppActivity.java
@@ -0,0 +1,401 @@
+/*
+ * Copyright (C) 2013 OSRF.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.github.rosjava.android_apps.application_management;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.github.rosjava.android_apps.application_management.rapp_manager.PairingApplicationNamePublisher;
+
+import org.ros.address.InetAddressFactory;
+import org.ros.android.RosActivity;
+import org.ros.exception.RemoteException;
+import org.ros.exception.RosRuntimeException;
+import org.ros.namespace.NameResolver;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+import org.ros.node.service.ServiceResponseListener;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
+
+import rocon_app_manager_msgs.StartAppResponse;
+import rocon_app_manager_msgs.StopAppResponse;
+
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author murase@jsk.imi.i.u-tokyo.ac.jp (Kazuto Murase)
+ * 
+ * This is a clone of RosAppActivity adapted for concert remocon apps; TODO: unify
+ */
+public abstract class ConcertAppActivity extends RosActivity {
+
+	private String robotAppName = null;
+	private String defaultRobotAppName = null;
+	private String defaultRobotName = null;
+    private String androidApplicationName; // descriptive helper only
+    /*
+      By default we assume the rosappactivity is launched independantly.
+      The following flags are used to identify when it has instead been
+      launched by a controlling application (e.g. remocons).
+     */
+    private boolean managedApplication = false;
+    private String managedApplicationActivity = null; // e.g. com.github.rosjava.android_remocons.robot_remocon.RobotRemocon
+    private PairingApplicationNamePublisher managedPairingApplicationNamePublisher;
+
+	private int dashboardResourceId = 0;
+	private int mainWindowId = 0;
+	private Dashboard dashboard = null;
+	private NodeConfiguration nodeConfiguration;
+	private NodeMainExecutor nodeMainExecutor;
+	private URI uri;
+	protected RobotNameResolver robotNameResolver;
+    protected ConcertDescription concertDescription;
+
+    // By now params and remaps are only available for concert apps; that is, concertApplication must be true
+    protected LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
+    protected LinkedHashMap<String, String> remaps = new LinkedHashMap<String, String>();
+
+	protected void setDashboardResource(int resource) {
+		dashboardResourceId = resource;
+	}
+
+	protected void setMainWindowResource(int resource) {
+		mainWindowId = resource;
+	}
+
+	protected void setDefaultRobotName(String name) {
+		defaultRobotName = name;
+	}
+
+	protected void setDefaultAppName(String name) {
+        defaultRobotAppName = name;
+	}
+
+	protected void setCustomDashboardPath(String path) {
+		dashboard.setCustomDashboardPath(path);
+	}
+
+	protected ConcertAppActivity(String notificationTicker, String notificationTitle) {
+		super(notificationTicker, notificationTitle);
+        this.androidApplicationName = notificationTitle;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (mainWindowId == 0) {
+			Log.e("ApplicationManagement",
+					"You must set the dashboard resource ID in your RosAppActivity");
+			return;
+		}
+		if (dashboardResourceId == 0) {
+			Log.e("ApplicationManagement",
+					"You must set the dashboard resource ID in your RosAppActivity");
+			return;
+		}
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(mainWindowId);
+
+		robotNameResolver = new RobotNameResolver();
+
+		if (defaultRobotName != null) {
+			robotNameResolver.setRobotName(defaultRobotName);
+		}
+
+        robotAppName = getIntent().getStringExtra(AppManager.PACKAGE + ".concert_app_name");
+        if (robotAppName == null) {
+            robotAppName = defaultRobotAppName;
+
+		} else {
+            managedApplicationActivity = getIntent().getStringExtra("PairedManagerActivity");
+            managedPairingApplicationNamePublisher = new PairingApplicationNamePublisher(this.androidApplicationName);
+			managedApplication = true;
+
+            // Extract parameters and remappings from a YAML-formatted strings; translate into hash maps
+            Yaml yaml = new Yaml();
+
+            String paramsStr = getIntent().getStringExtra("Parameters");
+            if (paramsStr != null) {
+                params = (LinkedHashMap<String, Object>) yaml.load(paramsStr);
+                Log.d("ApplicationManagement", "Parameters: " + paramsStr);
+            }
+
+            String remapsStr = getIntent().getStringExtra("Remappings");
+            if (remapsStr != null) {
+                remaps = (LinkedHashMap<String, String>) yaml.load(remapsStr);
+                Log.d("ApplicationManagement", "Remappings: " + remapsStr);
+            }
+        }
+
+        if (dashboard == null) {
+			dashboard = new Dashboard(this);
+			dashboard.setView((LinearLayout) findViewById(dashboardResourceId),
+					new LinearLayout.LayoutParams(
+							LinearLayout.LayoutParams.WRAP_CONTENT,
+							LinearLayout.LayoutParams.WRAP_CONTENT));
+		}
+
+	}
+
+	@Override
+	protected void init(NodeMainExecutor nodeMainExecutor) {
+		this.nodeMainExecutor = nodeMainExecutor;
+		nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory
+                .newNonLoopback().getHostAddress(), getMasterUri());
+
+        if ( managedApplication ) {
+            if (getIntent().hasExtra(ConcertDescription.UNIQUE_KEY)) {
+                concertDescription = (ConcertDescription) getIntent()
+                        .getSerializableExtra(ConcertDescription.UNIQUE_KEY);
+            }
+//            nodeMainExecutor.execute(managedPairingApplicationNamePublisher,
+//                    nodeConfiguration.setNodeName("pairingApplicationNamePublisher"));
+        }
+//		if (concertDescription != null) {
+//            robotNameResolver.setRobot(concertDescription);
+//			dashboard.setRobotName(concertDescription.getRobotType());
+//		}
+//		nodeMainExecutor.execute(robotNameResolver,
+//				nodeConfiguration.setNodeName("robotNameResolver"));
+//        robotNameResolver.waitForResolver();
+//
+//        if (concertDescription == null) {
+//            dashboard.setRobotName(getRobotNameSpace().getNamespace()
+//                .toString());
+//        }
+
+//        nodeMainExecutor.execute(dashboard,
+//				nodeConfiguration.setNodeName("dashboard"));
+
+
+        // probably need to reintegrate this restart mechanism
+//        if (managedApplication && startRobotApplication) {
+//			if (getIntent().getBooleanExtra("runningNodes", false)) {
+//				restartApp();
+        if ( managePairedRobotApplication() ) {
+            Log.w("ApplicationManagement", "starting a rapp");
+            startApp();
+        }
+    }
+
+	protected NameResolver getAppNameSpace() {
+		return robotNameResolver.getAppNameSpace();
+	}
+
+	protected NameResolver getRobotNameSpace() {
+		return robotNameResolver.getRobotNameSpace();
+	}
+
+	protected void onAppTerminate() {
+		com.github.rosjava.android_apps.application_management.ConcertAppActivity.this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				new AlertDialog.Builder(com.github.rosjava.android_apps.application_management.ConcertAppActivity.this)
+						.setTitle("App Termination")
+						.setMessage(
+								"The application has terminated on the server, so the client is exiting.")
+						.setCancelable(false)
+						.setNeutralButton("Exit",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int which) {
+                                        com.github.rosjava.android_apps.application_management.ConcertAppActivity.this.finish();
+									}
+								}).create().show();
+			}
+		});
+	}
+
+	@Override
+	public void startMasterChooser() {
+		if (!managedApplication) {
+			super.startMasterChooser();
+		} else {
+			Intent intent = new Intent();
+			intent.putExtra(AppManager.PACKAGE + ".robot_app_name",
+					"AppChooser");
+			try {
+				uri = new URI(getIntent().getStringExtra("ChooserURI"));
+			} catch (URISyntaxException e) {
+				throw new RosRuntimeException(e);
+			}
+
+			nodeMainExecutorService.setMasterUri(uri);
+			new AsyncTask<Void, Void, Void>() {
+				@Override
+				protected Void doInBackground(Void... params) {
+					com.github.rosjava.android_apps.application_management.ConcertAppActivity.this.init(nodeMainExecutorService);
+					return null;
+				}
+			}.execute();
+		}
+
+	}
+
+	private void restartApp() {
+		Log.i("RosAndroid", "Restarting application");
+		AppManager appManager = new AppManager("*", getRobotNameSpace());
+		appManager.setFunction("stop");
+
+		appManager
+				.setStopService(new ServiceResponseListener<StopAppResponse>() {
+					@Override
+					public void onSuccess(StopAppResponse message) {
+						Log.i("RosAndroid", "App stopped successfully");
+						try {
+							Thread.sleep(1000);
+						} catch (Exception e) {
+
+						}
+						startApp();
+					}
+
+					@Override
+					public void onFailure(RemoteException e) {
+						Log.e("RosAndroid", "App failed to stop!");
+					}
+				});
+		nodeMainExecutor.execute(appManager,
+				nodeConfiguration.setNodeName("start_app"));
+
+	}
+
+    /**
+     * This is currently only used by apps that start themselves. Apps launched by an external
+     * program (e.g. remocons) will do their own handling of the appmanager.
+     */
+	private void startApp() {
+		Log.i("ApplicationManagement", "android application starting a rapp [" + robotAppName + "]");
+
+		AppManager appManager = new AppManager(robotAppName,
+				getRobotNameSpace());
+		appManager.setFunction("start");
+
+		appManager
+				.setStartService(new ServiceResponseListener<StartAppResponse>() {
+					@Override
+					public void onSuccess(StartAppResponse message) {
+						if (message.getStarted()) {
+							Log.i("ApplicationManagement", "rapp started successfully [" + robotAppName + "]");
+						} else {
+							Log.e("ApplicationManagement", "rapp failed to start! [" + message.getMessage() + "]");
+                        }
+					}
+
+					@Override
+					public void onFailure(RemoteException e) {
+						Log.e("ApplicationManagement", "rapp failed to start - no response!");
+					}
+				});
+
+		nodeMainExecutor.execute(appManager,
+				nodeConfiguration.setNodeName("start_app"));
+	}
+
+	protected void stopApp() {
+		Log.i("ApplicationManagement", "android application stopping a rapp [" + robotAppName + "]");
+		AppManager appManager = new AppManager(robotAppName,
+				getRobotNameSpace());
+		appManager.setFunction("stop");
+
+		appManager
+				.setStopService(new ServiceResponseListener<StopAppResponse>() {
+					@Override
+					public void onSuccess(StopAppResponse message) {
+                        if ( message.getStopped() ) {
+						    Log.i("ApplicationManagement", "App stopped successfully");
+                        } else {
+                            Log.i("ApplicationManagement", "Stop app request rejected [" + message.getMessage() + "]");
+                        }
+					}
+
+					@Override
+					public void onFailure(RemoteException e) {
+						Log.e("ApplicationManagement", "App failed to stop when requested!");
+					}
+				});
+		nodeMainExecutor.execute(appManager,
+				nodeConfiguration.setNodeName("stop_app"));
+	}
+
+	protected void releaseRobotNameResolver() {
+		nodeMainExecutor.shutdownNodeMain(robotNameResolver);
+	}
+
+	protected void releaseDashboardNode() {
+		nodeMainExecutor.shutdownNodeMain(dashboard);
+	}
+
+    /**
+     * Whether this ros app activity should be responsible for
+     * starting and stopping a paired robot application.
+     *
+     * This responsibility is relinquished if the application
+     * is controlled from a remocon, but required if the
+     * android application is connecting and running directly.
+     *
+     * @return boolean : true if it needs to be managed.
+     */
+    private boolean managePairedRobotApplication() {
+        return (!managedApplication && (robotAppName != null));
+    }
+
+	@Override
+	protected void onDestroy() {
+        if ( ( getRobotNameSpace() != null ) && managePairedRobotApplication() ) {
+			stopApp();
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (managedApplication) {
+            Log.i("ApplicationManagement", "app terminating and returning control to the remocon.");
+            // Restart the remocon, supply it with the necessary information and stop this activity
+			Intent intent = new Intent();
+			intent.putExtra(AppManager.PACKAGE + ".concert_app_name",
+					"AppChooser");
+            intent.putExtra(ConcertDescription.UNIQUE_KEY, concertDescription);
+			intent.putExtra("ChooserURI", uri.toString());
+//            intent.putExtra("RobotType",concertDescription.getRobotType());
+//            intent.putExtra("RobotName",concertDescription.getRobotName());
+            intent.setAction(managedApplicationActivity);
+            //intent.setAction("com.github.robotics_in_concert.rocon_android.robot_remocon.RobotRemocon");
+			intent.addCategory("android.intent.category.DEFAULT");
+			startActivity(intent);
+			finish();
+		} else {
+            Log.i("ApplicationManagement", "backpress processing for RosAppActivity");
+        }
+		super.onBackPressed();
+	}
+}
---
+++ application_management/src/main/java/com/github/rosjava/android_apps/application_management/ConcertDescription.java
@@ -0,0 +1,236 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *  * Neither the name of Willow Garage, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.github.rosjava.android_apps.application_management;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConcertDescription implements java.io.Serializable {
+    public static final String CONNECTING = "connecting...";
+    public static final String OK = "ok";
+    public static final String ERROR = "exception";
+    public static final String WIFI = "invalid wifi";
+    public static final String UNAVAILABLE = "unavailable";  // when the concert app manager is busy serving another remote controller
+    public static final String CONTROL = "not started";
+    private static final String NAME_UNKNOWN = "Unknown";
+    private static final String TYPE_UNKNOWN = "Unknown";
+    private static final long serialVersionUID = 1L;
+    private ConcertId concertId;
+    private String concertName;
+    private String description;
+    private String[] userRoles;
+    private int currentRole = -1;
+    // Icon stored piecemeal because msg arrays (stored as jboss ChannelBuffers) can't
+    // be dumped and reloaded by the snakeyaml library.
+    private String concertIconFormat;
+    private byte[] concertIconData;
+    private int concertIconDataOffset;
+    private int concertIconDataLength;
+
+    private String connectionStatus;
+    private Date timeLastSeen;
+    // Unique identifier for keys passed between android apps.
+    public static final String UNIQUE_KEY = "com.github.rosjava.android_apps.application_management.ConcertDescription";
+
+    // TODO(kwc): add in canonicalization of concertName
+    public ConcertDescription() {
+    }
+
+    public ConcertDescription(ConcertId concertId, String concertName, String description,
+                              rocon_std_msgs.Icon concertIcon, Date timeLastSeen) {
+            setConcertName(concertName);
+            setDescription(description);
+            setConcertId(concertId);
+            this.concertName = concertName;
+            if ( concertIcon != null ) {
+                this.concertIconFormat = concertIcon.getFormat();
+                this.concertIconData = concertIcon.getData().array();
+                this.concertIconDataOffset = concertIcon.getData().arrayOffset();
+                this.concertIconDataLength = concertIcon.getData().readableBytes();
+            }
+            this.timeLastSeen = timeLastSeen;
+    }
+    public void copyFrom(ConcertDescription other) {
+            concertId = other.concertId;
+            userRoles = other.userRoles.clone();
+            concertName = other.concertName;
+            description = other.description;
+            concertIconFormat = other.concertIconFormat;
+            concertIconData = other.concertIconData;
+            concertIconDataOffset = other.concertIconDataOffset;
+            concertIconDataLength = other.concertIconDataLength;
+            connectionStatus = other.connectionStatus;
+            timeLastSeen = other.timeLastSeen;
+    }
+    public ConcertId getConcertId() { return concertId; }
+    public String getDescription()  { return description; }
+    public String getConcertName()  { return concertName; }
+    public String[] getUserRoles()  { return userRoles;}
+    public String getCurrentRole()  {
+        if (userRoles != null && currentRole >= 0 && currentRole <  userRoles.length)
+            return userRoles[currentRole];
+        else
+            return null;
+    }
+
+    /**
+     * Convenience accessor to dig into the master uri for this concert.
+     * @return String : the ros master uri for this concert.
+     */
+    public String getMasterUri() {
+        return concertId.getMasterUri();
+    }
+
+    public void setConcertId(ConcertId concertId) {
+            // TODO: ensure the concert id is sane.
+//              if(false) {
+//                      throw new InvalidConcertDescriptionException("Empty Master URI");
+//              }
+            // TODO: validate
+            this.concertId = concertId;
+    }
+    public void setUserRoles(concert_msgs.Roles roles)
+    {
+        java.util.List<String> tmp = roles.getList();
+        userRoles = new String[tmp.size()];
+        tmp.toArray(userRoles);
+    }
+    public void setCurrentRole(int role) { currentRole = role; }
+
+    /** Provide a human-friendly interpretation of the concert name
+     *
+     * Often we use concert names with a uuid suffix to ensure the concert
+     * name in a multi-concert group is unique. This checks for the
+     * suffix and if found, strips it.
+     *
+     * It also converts '_'s to spaces and first character of each word
+     * to uppercase.
+     *
+     * @return human friendly string name
+     */
+    public String getConcertFriendlyName() {
+        String friendlyName = concertName;
+        // The uuid is a 16 byte hash in hex format = 32 characters
+        if (concertName.length() > 32) {
+            String possibleUuidPart = concertName.substring(concertName.length() - 32);
+            Pattern p = Pattern.compile("[^a-f0-9]");
+            Matcher m = p.matcher(possibleUuidPart);
+            if (!m.find()) {
+                friendlyName = concertName.substring(0, concertName.length() - 32);
+            }
+        }
+        friendlyName = friendlyName.replace('_',' ');
+        final StringBuilder result = new StringBuilder(friendlyName.length());
+        String[] words = friendlyName.split("\\s");
+        for(int i=0, l=words.length; i<l; ++i) {
+          if(i > 0) result.append(" ");
+          result.append(Character.toUpperCase(words[i].charAt(0)))
+                .append(words[i].substring(1));
+        }
+        return result.toString();
+    }
+
+    public void setConcertName(String concertName) { this.concertName = concertName; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getConcertIconFormat() {
+        return concertIconFormat;
+    }
+
+    public ChannelBuffer getConcertIconData() {
+        if ( concertIconData == null ) {
+            return null;
+        } else {
+            ChannelBuffer channelBuffer = ChannelBuffers.copiedBuffer(concertIconData, concertIconDataOffset, concertIconDataLength);
+            return channelBuffer;
+        }
+    }
+
+    public void setConcertIcon(rocon_std_msgs.Icon concertIcon) {
+        this.concertIconFormat = concertIcon.getFormat();
+        this.concertIconData = concertIcon.getData().array();
+    }
+
+    public String getConnectionStatus() {
+            return connectionStatus;
+    }
+    public void setConnectionStatus(String connectionStatus) {
+            this.connectionStatus = connectionStatus;
+    }
+    public Date getTimeLastSeen() {
+            return timeLastSeen;
+    }
+    public void setTimeLastSeen(Date timeLastSeen) {
+            this.timeLastSeen = timeLastSeen;
+    }
+    public boolean isUnknown() {
+            return this.concertName.equals(NAME_UNKNOWN);
+    }
+    public static ConcertDescription createUnknown(ConcertId concertId)  {
+            return new ConcertDescription(concertId, NAME_UNKNOWN, TYPE_UNKNOWN, null, new Date());
+    }
+    @Override
+    public boolean equals(Object o) {
+            // Return true if the objects are identical.
+            // (This is just an optimization, not required for correctness.)
+            if(this == o) {
+                    return true;
+            }
+            // Return false if the other object has the wrong type.
+            // This type may be an interface depending on the interface's
+            // specification.
+            if(!(o instanceof ConcertDescription)) {
+                    return false;
+            }
+            // Cast to the appropriate type.
+            // This will succeed because of the instanceof, and lets us access private fields.
+            ConcertDescription lhs = (ConcertDescription) o;
+            // Check each field. Primitive fields, reference fields, and nullable reference
+            // fields are all treated differently.
+            return (concertId == null ? lhs.concertId == null : concertId.equals(lhs.concertId));
+    }
+    // I need to override equals() so I'm also overriding hashCode() to match.
+    @Override
+    public int hashCode() {
+            // Start with a non-zero constant.
+            int result = 17;
+            // Include a hash for each field checked by equals().
+            result = 31 * result + (concertId == null ? 0 : concertId.hashCode());
+            return result;
+    }
+}
