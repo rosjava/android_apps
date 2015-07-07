42d303a8385ce756b969ad9c528feed8271fb370
Jihoon Lee <jihoonlee.in@gmail.com>
2015-07-07 11:41:28 +0900

    reformatting code with android studio

--- teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
+++ teleop/src/main/java/com/github/rosjava/android_apps/teleop/MainActivity.java
@@ -38,22 +38,22 @@ import java.io.IOException;
  * @author murase@jsk.imi.i.u-tokyo.ac.jp (Kazuto Murase)
  */
 public class MainActivity extends RosAppActivity {
-	private RosImageView<sensor_msgs.CompressedImage> cameraView;
-	private VirtualJoystickView virtualJoystickView;
-	private Button backButton;
+    private RosImageView<sensor_msgs.CompressedImage> cameraView;
+    private VirtualJoystickView virtualJoystickView;
+    private Button backButton;
 
-	public MainActivity() {
-		// The RosActivity constructor configures the notification title and ticker messages.
-		super("android teleop", "android teleop");
-	}
+    public MainActivity() {
+        // The RosActivity constructor configures the notification title and ticker messages.
+        super("android teleop", "android teleop");
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		setDashboardResource(R.id.top_bar);
-		setMainWindowResource(R.layout.main);
-		super.onCreate(savedInstanceState);
+        setDashboardResource(R.id.top_bar);
+        setMainWindowResource(R.layout.main);
+        super.onCreate(savedInstanceState);
 
         cameraView = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.image);
         cameraView.setMessageType(sensor_msgs.CompressedImage._TYPE);
@@ -66,51 +66,51 @@ public class MainActivity extends RosAppActivity {
                 onBackPressed();
             }
         });
-	}
+    }
 
-	@Override
-	protected void init(NodeMainExecutor nodeMainExecutor) {
-		
-		super.init(nodeMainExecutor);
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+
+        super.init(nodeMainExecutor);
 
         try {
-			NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getMasterUri());
+            NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getMasterUri());
 
-        	String joyTopic = remaps.get(getString(R.string.joystick_topic));
-        	String camTopic = remaps.get(getString(R.string.camera_topic));
+            String joyTopic = remaps.get(getString(R.string.joystick_topic));
+            String camTopic = remaps.get(getString(R.string.camera_topic));
 
-        	NameResolver appNameSpace = getMasterNameSpace();
-        	joyTopic = appNameSpace.resolve(joyTopic).toString();
-        	camTopic = appNameSpace.resolve(camTopic).toString();
+            NameResolver appNameSpace = getMasterNameSpace();
+            joyTopic = appNameSpace.resolve(joyTopic).toString();
+            camTopic = appNameSpace.resolve(camTopic).toString();
 
-			cameraView.setTopicName(camTopic);
-        	virtualJoystickView.setTopicName(joyTopic);
-		
-			nodeMainExecutor.execute(cameraView, nodeConfiguration.setNodeName("android/camera_view"));
-			nodeMainExecutor.execute(virtualJoystickView,
-			nodeConfiguration.setNodeName("android/virtual_joystick"));
+            cameraView.setTopicName(camTopic);
+            virtualJoystickView.setTopicName(joyTopic);
+
+            nodeMainExecutor.execute(cameraView, nodeConfiguration.setNodeName("android/camera_view"));
+            nodeMainExecutor.execute(virtualJoystickView,
+                    nodeConfiguration.setNodeName("android/virtual_joystick"));
         } catch (IOException e) {
             // Socket problem
-			Log.e("Teleop", "Error while configuring node " + e);
-		}
+            Log.e("Teleop", "Error while configuring node " + e);
+        }
 
-	}
-	
-	  @Override
-	  public boolean onCreateOptionsMenu(Menu menu){
-		  menu.add(0,0,0,R.string.stop_app);
+    }
 
-		  return super.onCreateOptionsMenu(menu);
-	  }
-	  
-	  @Override
-	  public boolean onOptionsItemSelected(MenuItem item){
-		  super.onOptionsItemSelected(item);
-		  switch (item.getItemId()){
-		  case 0:
-			  onDestroy();
-			  break;
-		  }
-		  return true;
-	  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, R.string.stop_app);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case 0:
+                onDestroy();
+                break;
+        }
+        return true;
+    }
 }
